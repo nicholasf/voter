03b6c6df0a97798d577ee0eeda4764d398b80d97
Nicholas Faiz <nicholas.faiz@gmail.com>
2013-08-10 09:12:56 +1000

    cleaning out some logging statements'

--- controllers/voting_topic_controller.js
+++ controllers/voting_topic_controller.js
@@ -1,11 +1,8 @@
 var db = require('../db');
 
 exports.show = function(req, res){
-  console.log('ok .... ', req.params);
   var votingTopicId = req.params['votingTopicId'];
   var votingTopic = db.votingTopics[votingTopicId];
-  console.log('found it', votingTopic);
-  console.log('the db: ', db);
 
   res.render('voting_topics/show', {votingTopic: votingTopic});
 }

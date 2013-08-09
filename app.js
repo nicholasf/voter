234462600077fc08dd81b12db3b03f52b497659c
Nicholas Faiz <nicholas.faiz@gmail.com>
2013-08-09 21:49:56 +1000

    refactoring how we setup our routes; app.js now passes the app instance into the routes/index.js, which manages wiring HTTP behaviours to controller functions

--- app.js
+++ app.js
@@ -4,11 +4,21 @@
 
 var express = require('express')
   , routes  = require('./routes')
-  , user    = require('./routes/user')
   , http    = require('http')
   , path    = require('path')
   , env     = require('./conf/env')
-  , stylus  = require('stylus');
+  , stylus  = require('stylus')
+  , sockjs  = require('sockjs');
+
+var echo = sockjs.createServer();
+echo.on('connection', function(conn) {
+    conn.on('data', function(message) {
+        console.log("ok ...", message);
+        console.log(arguments);
+        conn.write(message);
+    });
+    conn.on('close', function() {});
+});
 
 var paths = {
   stylus: path.join(__dirname, 'public', 'stylesheets'),
@@ -35,9 +45,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes.setup(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+server = http.createServer(app);
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
+var welcomer  = require('./../controllers/welcome_controller')
+var users     = require('./../controllers/users_controller')
+
+exports.setup = function(app) {
+  app.get('/', welcomer.home);
+  // app.get('/users', user.list);
+}
+
 
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
